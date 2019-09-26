e189ee2b4d5efe81ba95e90e8b5860b850349e2a
Your Name <you@example.com>
2019-09-27 03:10:24 +0900

    mod

--- update.sh
+++ update.sh
@@ -4,12 +4,10 @@ wget -N https://280blocker.net/files/280blocker_adblock.txt
 wget -N https://filters.adtidy.org/extension/chromium/filters/7.txt
 wget -N https://filters.adtidy.org/extension/chromium/filters/2.txt
 wget -N https://easylist.to/easylist/easylist.txt
+wget -N https://easylist.to/easylist/easyprivacy.txt
 wget -N https://raw.githubusercontent.com/k2jp/abp-japanese-filters/master/abpjf.txt
 wget -N https://raw.githubusercontent.com/k2jp/abp-japanese-filters/master/abpjf_element_hiding.txt
-#wget -N http://tofukko.r.ribbon.to/Adblock_Plus_list.txt
-#wget -N http://tofukko.r.ribbon.to/Adblock_Plus_list_sjis.txt
 wget -N https://raw.githubusercontent.com/tofukko/filter/master/Adblock_Plus_list.txt
-#wget -N http://www.malwaredomainlist.com/hostslist/hosts.txt
 wget -N https://www.malwaredomainlist.com/hostslist/hosts.txt
 wget -N https://hosts-file.net/ad_servers.txt
 wget -N https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
